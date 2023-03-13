a4ac3626d4a88cab52701a8a950fb4eeab73316c
pinkjagnie <olga.krzysztofik@gmail.com>
2023-03-13 20:11:13 +0100

    Added Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Link from "next/link";
+
+import { SiGithub, SiLinkedin } from 'react-icons/si';
+
+const Navbar = () => {
+  return(
+    <section className="fixed left-0 top-0 w-full h-[9vh] z-10 bg-[#d66382] text-zinc-100">
+      <div className="flex justify-around">
+        <div className="flex items-center justify-center">
+          <h1 className="font-bold text-lg leading-[9vh]">olga krzysztofik</h1>
+          <h1 className="hidden lg:visible lg:block lg:font-bold lg:text-lg lg:leading-[9vh]"> - pink jagnie</h1>
+        </div>
+        <div className="flex items-center justify-between">
+          <Link href="https://github.com/pinkjagnie" target="_blank" rel="noopener noreferrer" className="px-2"><SiGithub size={20} /></Link>
+          <Link href="https://www.linkedin.com/in/olga-krzysztofik/" target="_blank" rel="noopener noreferrer" className="px-2"><SiLinkedin size={20} /></Link>
+        </div>
+      </div>
+    </section>
+  )
+};
+
+export default Navbar;
