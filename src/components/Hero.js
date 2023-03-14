987831e935c7a45410bcef31602af5dcf1225c81
pinkjagnie <olga.krzysztofik@gmail.com>
2023-03-14 11:52:23 +0100

    Changed photo in Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -14,11 +14,11 @@ const Hero = () => {
   return(
     <section id="hero" className="min-h-screen pt-[9vh] bg-zinc-100 lg:flex">
 
-      <div className="flex flex-col justify-center items-center w-[90%] mx-auto pt-8 lg:w-[30%] lg:items-start lg:pt-0">
+      <div className="flex flex-col justify-center items-center w-[90%] mx-auto pt-8 lg:w-[30%] lg:items-start lg:pt-0 xl:pl-20">
         <p className="lg:text-2xl">Cześć!</p>
         <p className="text-3xl pt-2 lg:text-5xl">Nazywam się</p>
         <div className="relative lg:w-[150%]">
-          <p className="absolute top-2 -left-[10%] h-[30px] w-[120%] bg-[#f7bbcb] lg:h-[48px] lg:w-[100%]"></p>
+          <p className="absolute top-2 -left-[10%] h-[30px] w-[120%] bg-[#f7bbcb] lg:h-[48px] lg:w-[100%] xl:w-[120%]"></p>
           <p className="text-3xl relative z-10 lg:text-5xl">Olga Krzysztofik</p>
         </div>
         <p className="text-center mx-auto pt-8 lg:text-xl lg:text-left">Jestem frontend developerką z różnorodnym doświadczeniem</p>
